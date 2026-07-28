810f4a7192d00b07a1c63631e0be369833552841
Joseph Gibson <Joseph.Gibson@ukhsa.gov.uk>
2026-07-28 10:56:31 +0100

    pygom init file

--- src/pygom/__init__.py
+++ src/pygom/__init__.py
@@ -3,11 +3,23 @@
 .. moduleauthor:: Edwin Tye <Edwin.Tye@phe.gov.uk>
 
 '''
-# from pkg_resources import get_distribution, DistributionNotFound
+
+from importlib.metadata import PackageNotFoundError, version
 
 from .loss import *
 from .model import *
-#from .utilR import *
+# from .utilR import *
+
+try:
+    __version__ = version("pygom")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+# from pkg_resources import get_distribution, DistributionNotFound
+
+# from .loss import *
+# from .model import *
+# #from .utilR import *
 
 # try:
 #     __version__ = get_distribution(__name__).version
