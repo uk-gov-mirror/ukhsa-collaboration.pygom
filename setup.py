41b3987efe43f011bf6438b810a8712ae6aeefbe
Joseph Gibson <Joseph.Gibson@ukhsa.gov.uk>
2026-07-28 10:56:06 +0100

    Update installation

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-#!/bin/env python
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-from Cython.Compiler import Options
-import numpy
+from setuptools import setup
 
-# Options for the compiled modules
-Options.docstrings = True
-Options.annotate = False
+setup()
 
-# extensions = [
-#     Extension("pygom.model._tau_leap",
-#               ["src/pygom/model/_tau_leap.pyx"],
-#               include_dirs=[numpy.get_include()],
-#               extra_compile_args=['-std=c99'],
-# #                  extra_compile_args=['-fopenmp'],
-# #                  extra_link_args=['-fopenmp']),
-#              )
-# ]
+# #!/bin/env python
+# from setuptools import setup, Extension
+# from Cython.Build import cythonize
+# from Cython.Compiler import Options
+# import numpy
 
-extensions = []
+# # Options for the compiled modules
+# Options.docstrings = True
+# Options.annotate = False
 
-setup(ext_modules=cythonize(extensions, compiler_directives={"language_level": 3, "profile": False}))
+# # extensions = [
+# #     Extension("pygom.model._tau_leap",
+# #               ["src/pygom/model/_tau_leap.pyx"],
+# #               include_dirs=[numpy.get_include()],
+# #               extra_compile_args=['-std=c99'],
+# # #                  extra_compile_args=['-fopenmp'],
+# # #                  extra_link_args=['-fopenmp']),
+# #              )
+# # ]
+
+# extensions = []
+
+# setup(ext_modules=cythonize(extensions, compiler_directives={"language_level": 3, "profile": False}))
